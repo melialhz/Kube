5e9e96b55501f3d2ae50f8adae69ac288e50a0f1
milia <milia@MacBook-Air-de-milia.local>
2026-01-06 03:36:09 +0100

    k

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,19 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-// Simulation de données provenant de MongoDB
-const utilisateursEnBase = [
+// On récupère l'URL de MongoDB depuis les variables d'environnement de K8s
+// Si elle n'existe pas, on met une valeur par défaut
+const mongoUrl = process.env.DB_URL || "mongodb://admin:password123@mongodb-service:27017/admin";
 
-];
+// --- LA PARTIE IMPORTANTE : LE .catch() ---
+mongoose.connect(mongoUrl)
+  .then(() => console.log("✅ Connexion à MongoDB réussie !"))
+  .catch(err => {
+    console.error("⚠️ Erreur de connexion MongoDB :");
+    console.error(err.message);
+    console.log("🚀 Le serveur continue de tourner sans BDD pour l'instant...");
+  });
+// ------------------------------------------
 
 app.get('/api/message', (req, res) => {
   res.json({ 
-    message: "Connecté au Cluster Kubernetes (Simulé)",
-    database: "MongoDB : Status OK",
-    items: utilisateursEnBase 
+    message: "Hello ! Le Backend est en ligne !",
+    database: mongoose.connection.readyState === 1 ? "Connectée" : "Déconnectée"
   });
 });
 
-app.listen(5000, () => {
-  console.log("🚀 Serveur de simulation démarré sur http://localhost:5000");
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Serveur démarré sur le port ${PORT}`);
 });
